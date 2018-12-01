625db910f06969ec32bf46c07178a2c17defd671
QuocAnh-ChrisD <shadow13000>
2018-12-01 17:23:10 +0100

    NotSupportedException -> créer

---
+++ src/main/java/image/NotSupportedException.java
@@ -0,0 +1,9 @@
+package image;
+
+public class NotSupportedException extends RuntimeException{
+
+    public NotSupportedException(){
+        System.out.println("Not Supported Exception !");
+    }
+
+}
